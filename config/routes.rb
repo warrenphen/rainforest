57d6b295a848efcb8294e74f2760adfdc4775501
Warren Phen <warren.phen@gmail.com>
2015-04-22 15:44:19 -0400

    Writing the review routes, controllers and views. Added product links and link to create product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :products
+  root :to => "products#index"
+
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
