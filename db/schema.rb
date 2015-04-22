aec9c0628a0ac36245e33c3ad2fcfa04dfe05f0b
Warren Phen <warren.phen@gmail.com>
2015-04-22 16:04:10 -0400

    Adding user names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421001841) do
+ActiveRecord::Schema.define(version: 20150422195318) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150421001841) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
