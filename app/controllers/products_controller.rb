844c56e229f382b546b097c0a89f0085e5315b38
Warren Phen <warren.phen@gmail.com>
2015-05-05 23:55:08 -0400

    added search feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def index
-  	@products = Product.all
+  	@products = if params[:search]
+        Product.where("Lower(name) LIKE LOWER(?)", "%#{params[:search]}%")
+      else
+        Product.all
+      end
   end
 
   def show
