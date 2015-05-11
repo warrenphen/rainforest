15c554b08867311a42b3b69c61dceca9716fd6ff
Warren Phen <warren.phen@gmail.com>
2015-05-10 23:21:12 -0400

    edited review form to post via AJAX and edited review controller to respond to AJAX

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,11 +11,16 @@ class ReviewsController < ApplicationController
   	@review.user = current_user
   
 
-	  if @review.save
-	  	redirect_to products_path, notice: 'Review created successfully'
-	  else
-	  	render 'products/show'
-	  end
+	  respond_to do |format|
+      if @review.save
+        format.html { redirect_to product_path(@product.id), notice: 'Review added.' }
+        format.js {} # This will look for app/views/reviews/create.js.erb
+      else
+        format.html { render 'products/show', alert: 'There was an error.'  }
+        format.js {} # This will look for app/views/reviews/create.js.erb
+      end
+    end   
+    
 	end
 
   def destroy
